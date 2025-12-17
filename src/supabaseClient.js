6a83b1ff8998c3958bdd95f45fb723e26f907745
CarterIsrael17 <adetunjiisrael2@gmail.com>
2025-12-17 09:29:45 +0100

    supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://jikopeeidvhzkzqqvzco.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppa29wZWVpZHZoemt6cXF2emNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NjMyODgsImV4cCI6MjA3OTAzOTI4OH0.117K6kQzB89kMBW94CMUbm5LiNue6Mnd7g7OSXFEr4U";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
