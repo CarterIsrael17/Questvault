600b75b7b8c6855cdae7c3b39dea56ed561ac0f5
CarterIsrael17 <adetunjiisrael2@gmail.com>
2025-12-17 14:43:54 +0100

    supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
-// src/supabase.js
-import { createClient } from "@supabase/supabase-js";
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-// Hardcoded Supabase config (frontend safe)
-const SUPABASE_URL = "https://jikopeeidvhzkzqqvzco.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppa29wZWVpZHZoemt6cXF2emNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NjMyODgsImV4cCI6MjA3OTAzOTI4OH0.117K6kQzB89kMBW94CMUbm5LiNue6Mnd7g7OSXFEr4U";
+const supabaseUrl = 'https://hpslhunwcwohletjtmxu.supabase.co';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhwc2xodW53Y3dvaGxldGp0bXh1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NTk3NjE0NywiZXhwIjoyMDgxNTUyMTQ3fQ.x8Q8zzjfKagIC90i21quMLgHTsbQEnmVUzsAyK0mMv4';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
